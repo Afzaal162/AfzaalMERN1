2090d1702f82993b98c9182f842abed65986bc2b
Afzaal Hassan <145698503+Afzaal162@users.noreply.github.com>
2025-12-04 22:52:17 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ import connectDB from "./config/mongodb.js";
 import authRouter from "./route/authRoutes.js";
 
 const app = express();
-const port = process.env.PORT || 4000;
 
 // Connect Database
 connectDB();
@@ -17,16 +16,14 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001"], // allow both ports
-  credentials: true, // Allow cookies to be sent
+  origin: ["http://localhost:3000", "http://localhost:3001"],
+  credentials: true,
 }));
-
 
 // Routes
 app.get("/", (req, res) => res.send("API IS WORKING"));
 app.use("/api/auth", authRouter);
 
-// Start server
-app.listen(port, () =>
-  console.log(`Server Has Been Activated on PORT ${port}`)
-);
+// ❌ REMOVE app.listen()
+// ✔ EXPORT the app for Vercel
+export default app;
